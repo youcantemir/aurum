fdbb1a550db7b8b857507320b82ea8905fbd0f04
Yuliya <youcant@ukr.net>
2026-07-30 17:03:42 +0300

    Add AlarmConfig class for alarm system configuration

---
+++ src/main/java/simulator/config/AlarmConfig.java
@@ -0,0 +1,14 @@
+package simulator.config;
+
+public class AlarmConfig {
+
+    public static final String SYSTEM_NAME =
+            "Smart Alarm";
+
+    public static final boolean ENABLE_NOTIFICATIONS =
+            true;
+
+    public static final boolean SAVE_HISTORY =
+            true;
+
+}
